b924bf657b9adbac1d82f5a265b2b1e4bb5356a8
huang12zheng <805104533@qq.com>
2021-11-16 23:05:09 +0800

    feat: :wheelchair: add a parentheses for file
    parentheses would make file easy to clink
    
    fix: #1

--- lib/process_file.dart
+++ lib/process_file.dart
@@ -8,7 +8,7 @@ import 'code.dart';
 import 'space.dart';
 
 void processFile(File file) {
-  print('Processing ${file.path}...');
+  print('Processing (${file.path})...');
   var content = file.readAsStringSync();
   var result = processContent(file, content);
 
