42370e8a40a70334a7cc83c5a53e4ef996b4c6f3
Felix Blaschke <felix@felix-blaschke.de>
2025-05-03 16:08:56 +0200

    Refactor test for consistent line endings and improve readability

--- test/macro1_test.dart
+++ test/macro1_test.dart
@@ -6,16 +6,21 @@ import 'package:test/test.dart';
 void main() {
   test('can handle example files', () {
     var content =
-        File('${Directory.current.path}/test/data/test1_input_markdown')
-            .readAsStringSync();
+        File(
+          '${Directory.current.path}/test/data/test1_input_markdown',
+        ).readAsStringSync();
 
     // only used as relative import path for code
     var contentFile = File('${Directory.current.path}/test/data/markdown.md');
 
     var expectedOutput =
-        File('${Directory.current.path}/test/data/test1_expected_output')
-            .readAsStringSync();
+        File(
+          '${Directory.current.path}/test/data/test1_expected_output',
+        ).readAsStringSync();
 
-    expect(processContent(contentFile, content), expectedOutput);
+    var actual =
+        processContent(contentFile, content).replaceAll('\r', '').trimRight();
+    var expected = expectedOutput.replaceAll('\r', '').trimRight();
+    expect(actual, expected);
   });
 }
